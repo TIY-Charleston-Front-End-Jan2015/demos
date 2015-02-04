a4459f344bdb5a029b1984bf61c85d8deef269d9
Calvin Webster <calweb@gmail.com>
2015-02-04 11:49:34 -0500

    ajax crud

---
+++ bookListing/main.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+  books.init();
+
+});
+
+
+var books = {
+
+  init: function () {
+    books.initStyling();
+    books.initEvents();
+
+  },
+  initStyling: function () {
+    books.renderBooks();
+  },
+  initEvents: function () {
+
+    $('section').on('click', '.showEditBook', function (event) {
+      event.preventDefault();
+      $(this).closest('article').find('.editBook').toggleClass('show');
+    });
+
+    $('section').on('submit', '.editBook', function (event) {
+      event.preventDefault();
+      var bookId = $(this).closest('article').data('bookid');
+      var editedBook = {
+        title: $(this).find('input[name="editTitle"]').val(),
+        author: $(this).find('input[name="editAuthor"]').val(),
+        synopsis: $(this).find('textarea[name="editSynopsis"]').val(),
+        bookCover: $(this).find('input[name="editBookCover"]').val()
+      };
+
+      books.updateBook(bookId, editedBook);
+
+
+    });
+
+    $('#createBook').on('submit', function (event) {
+      event.preventDefault();
+        var newBook = {
+          title: $(this).find('input[name="newTitle"]').val(),
+          author: $(this).find('input[name="newAuthor"]').val(),
+          synopsis: $(this).find('textarea[name="newSynopsis"]').val(),
+          bookCover: $(this).find('input[name="newBookCover"]').val()
+        };
+
+        books.createBook(newBook);
+    });
+
+    $('section').on('click', '.deleteBook', function (event) {
+      event.preventDefault();
+       var bookId = $(this).closest('article').data('bookid');
+       console.log(bookId);
+       books.deleteBook(bookId);
+    });
+
+  },
+  config: {
+    url: 'http://tiy-fee-rest.herokuapp.com/collections/sally',
+
+  },
+  render: function (data, tmpl, $el) {
+    var template = _.template(data, tmpl);
+
+    $el.append(template);
+  },
+  renderBooks: function () {
+    $.ajax({
+      url: books.config.url,
+      type: 'GET',
+      success: function (books) {
+        console.log(books);
+        var template = _.template($('#bookTmpl').html());
+        var markup = "";
+        books.forEach(function (item, idx, arr) {
+          markup += template(item);
+        });
+        console.log('markup is.....', markup);
+        $('section').html(markup);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+  },
+  createBook: function (book) {
+
+    $.ajax({
+      url: books.config.url,
+      data: book,
+      type: 'POST',
+      success: function (data) {
+        console.log(data);
+        books.renderBooks();
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+
+  },
+  deleteBook: function (id) {
+
+    $.ajax({
+      url: books.config.url + '/' + id,
+      type: 'DELETE',
+      success: function (data) {
+        console.log(data);
+        books.renderBooks();
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+
+
+
+  },
+  updateBook: function (id, book) {
+
+    $.ajax({
+      url: books.config.url + '/' + id,
+      data: book,
+      type: 'PUT',
+      success: function (data) {
+        console.log(data);
+        books.renderBooks();
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+
+
+  },
+
+};
